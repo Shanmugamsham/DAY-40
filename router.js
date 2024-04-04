af4caad4d3b84879e629c4086247d4455c95f5bd
Shanmugamsham <abdeshanmugam@gmail.com>
2024-04-04 10:31:43 +0700

    new

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 const router=require("express").Router()
 const usermdal=require("./database")
 const bcrypt = require("bcrypt");
- const nodemailer=require("nodemailer")
+//  const nodemailer=require("nodemailer")
 
 
 router.post("/create", async(req,res,next)=>{
@@ -33,41 +33,41 @@ router.post("/create", async(req,res,next)=>{
     
   })
 
-  router.post("/resetpassword",async(req,res)=>{
-    const {email} =req.body
-    const user=  await usermdal.findOne({email})
-    if(!user){
-      return res.status(404).json({messege:"user is not found"})
-    }
-    const token=Math.random().toString(36).slice(-8)
-    user.resetpassword=token
-    user.restpasswordexpire=Date.now()+360000
-    await user.save()
+  // router.post("/resetpassword",async(req,res)=>{
+  //   const {email} =req.body
+  //   const user=  await usermdal.findOne({email})
+  //   if(!user){
+  //     return res.status(404).json({messege:"user is not found"})
+  //   }
+  //   const token=Math.random().toString(36).slice(-8)
+  //   user.resetpassword=token
+  //   user.restpasswordexpire=Date.now()+360000
+  //   await user.save()
 
-    const transpor=nodemailer.createTransport({
-      service:"gmail",
-      auth:{
-        user:"abdeshandy@gmail.com",
-        pass:"",
-      }
-    })
+  //   const transpor=nodemailer.createTransport({
+  //     service:"gmail",
+  //     auth:{
+  //       user:"abdeshandy@gmail.com",
+  //       pass:"",
+  //     }
+  //   })
 
-    const messege={
-      from:"abdeshandy@gmail.com",
-      to:user.email,
-      subject:"password reset",
-      text:`this is yor password code ${token}`
-    }
+  //   const messege={
+  //     from:"abdeshandy@gmail.com",
+  //     to:user.email,
+  //     subject:"password reset",
+  //     text:`this is yor password code ${token}`
+  //   }
 
-    transpor.sendMail(messege,(err,info)=>{
-      if(err){
-        return res.status(404).json({messege:"something is wrong"})
-      }
-      res.status(200).json({messege:"reset password mail sent"})
-    })
+  //   transpor.sendMail(messege,(err,info)=>{
+  //     if(err){
+  //       return res.status(404).json({messege:"something is wrong"})
+  //     }
+  //     res.status(200).json({messege:"reset password mail sent"})
+  //   })
 
 
-  })
+  // })
   
 
 
